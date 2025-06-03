6f9444a56beb7fd801b2882306af99c4848158f1
Syed Mustafa  Hassan <mustafa@Syeds-MacBook-Pro.local>
2025-06-03 08:52:18 +0500

    first

--- socket-server.js
+++ socket-server.js
@@ -43,7 +43,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
   console.log("🔌 Socket.IO server running on port 3001");
 });
 
